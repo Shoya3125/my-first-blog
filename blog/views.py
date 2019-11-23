503c3af426e36457cba8c98baadcf6508b2e0e95
Shoya <Shoya>
2019-11-23 18:42:07 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import post
 
 # Create your views here.
 def post_list(request):
     posts=post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    """クエリセット（DB情報を抽出）"""
     return render(request,'blog/post_list.html',{'posts':posts})
+
+def post_detail(request,pk):
+    posts=get_object_or_404(post,pk=pk)
+    """クエリセット（DB情報を抽出）"""
+    return render(request,'blog/post_detail.html',{'posts':posts})
